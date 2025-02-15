1ddbbe0146b2b0747873c78ba6c78ea5e573140e
Jciro17 <142267514+Jciro17@users.noreply.github.com>
2025-02-15 12:05:37 -0500

    Primer commit
    Archivos de spring initializer para adminucoshopprocessproductos

---
+++ src/main/java/com/adminucoshopprocessproductos/adminucoshopprocessproductos/AdminucoshopprocessproductosApplication.java
@@ -0,0 +1,13 @@
+package com.adminucoshopprocessproductos.adminucoshopprocessproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminucoshopprocessproductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminucoshopprocessproductosApplication.class, args);
+	}
+
+}
